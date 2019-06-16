4692c0a8afdf44e9b3b2d733eecc313965879d50
liweibin <382488114@qq.com>
2019-06-16 16:06:54 +0800

    这个是6月16日的lalalal文件

---
+++ lalala/web/src/main/java/com/itheima/web/maven_items_web.java
@@ -0,0 +1,11 @@
+package com.itheima.web;
+
+import com.itheima.domain.Items;
+import org.springframework.ui.Model;
+
+
+public interface maven_items_web {
+
+    public String findById(Model model);
+
+}
